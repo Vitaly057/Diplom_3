35b7ecd9b3ae5c96ae0c13dbbad1bca5ac55802c
Pechkunov Vitaly <witalink.viphaus@mail.ru>
2026-06-23 13:51:35 +0300

    Убраны прямые обращения к driver в тестах

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -1,8 +1,6 @@
 import allure
 from pages.account_page import AccountPage
 from pages.main_page import MainPage
-from urls import ACCOUNT_URL, LOGIN_URL, ORDER_HISTORY_URL
-
 @allure.feature('Личный кабинет')
 class TestPersonalAccount:
 
@@ -14,10 +12,6 @@ class TestPersonalAccount:
         main_page.click_account_button()
 
         assert account_page.is_profile_page_opened()
-        assert logged_in_driver.current_url.rstrip('/') in {
-            ACCOUNT_URL.rstrip('/'),
-            f'{ACCOUNT_URL.rstrip("/")}/profile',
-        }
 
     @allure.title('Переход в раздел «История заказов»')
     def test_transition_to_order_history(self, logged_in_driver):
@@ -28,7 +22,6 @@ class TestPersonalAccount:
         account_page.click_order_history_link()
 
         assert account_page.is_order_history_page_opened()
-        assert logged_in_driver.current_url == ORDER_HISTORY_URL
 
     @allure.title('Выход из аккаунта по кнопке «Выход»')
     def test_logout_from_account(self, logged_in_driver):
@@ -39,4 +32,3 @@ class TestPersonalAccount:
         account_page.click_logout_button()
 
         assert account_page.is_login_page_opened()
-        assert logged_in_driver.current_url == LOGIN_URL
